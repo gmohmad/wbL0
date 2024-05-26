e000b4b54f222979a4a05fe79321485acaec17d9
gmohmad <germihanovmohmad@gmail.com>
2024-05-26 16:08:22 +0300

    Add: some styling for index.html

--- static/script.js
+++ static/script.js
@@ -3,12 +3,10 @@ document.getElementById('orderForm').addEventListener('submit', async function(e
 	const orderId = document.getElementById('orderId').value;
 	const response = await fetch(`/order/${orderId}`);
 	const data = await response.json();
-	document.getElementById("status").innerText = data["status"]
+	console.log(data)
 	if (data["status"] === "OK") {
-		document.getElementById("orderItem").innerText = JSON.stringify(data["orderItem"], null, 2);
-		document.getElementById("errorMsg").innerText = "";
+		document.getElementById("resp").innerText = JSON.stringify(data["orderItem"], null, 2);
 	} else {
-		document.getElementById("orderItem").innerText = "";
-		document.getElementById("errorMsg").innerText = data["error"];
+		document.getElementById("resp").innerText = data["error"];
 	}
 });
